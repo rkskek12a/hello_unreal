82c6ed70d1fc5c0756ec436f1737d1c75eadd1c2
rkskek12a <ehowlqkd0514@naver.com>
2025-01-22 09:30:59 +0900

    Initial commit

---
+++ Source/hello_unreal/hello_unreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "hello_unreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, hello_unreal, "hello_unreal" );
